7fbcd06a637db6b1e7e0ae6fc9a4d8a38b21a50c
simon-macbook <simon.day91@hotmail.co.uk>
2019-12-31 11:45:38 +0800

    Add header component with nav links

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './components/Homepage';
 import ShopPage from './components/ShopPage';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Header />
         <Switch>
           <Route exact path="/" component={Homepage} />
           <Route path="/shop" component={ShopPage} />
