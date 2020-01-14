5e48bc7868d514f066769acb49f5b710639ca2c6
simon-macbook <simon.day91@hotmail.co.uk>
2020-01-14 17:37:41 +0800

    Add test  Stripe checkout to checkout page

--- src/components/pages/Checkout.js
+++ src/components/pages/Checkout.js
@@ -7,6 +7,7 @@ import {
   selectCartTotal
 } from '../../redux/cart/cartSelectors';
 import CheckoutItem from '../CheckoutItem';
+import StripeCheckoutButton from '../FormComponents/StripeButton';
 
 const Checkout = ({ cartItems, total }) => {
   return (
@@ -32,9 +33,13 @@ const Checkout = ({ cartItems, total }) => {
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
 
-      <div className="total">
-        <span>TOTAL: ${total}</span>
+      <div className="total">TOTAL: ${total}</div>
+      <div className="test-warning">
+        *Please use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
       </div>
+      <StripeCheckoutButton price={total} />
     </div>
   );
 };
